e45748cb1f8441cd7b5e50f85666a728858c8905
bertil291utn <bertilt4@gmail.com>
2020-05-06 18:00:58 -0500

    init comit - Added a example html page home.html.erb and css files - Play some routes in route file

---
+++ config/routes.rb
@@ -0,0 +1,5 @@
+Rails.application.routes.draw do
+  root 'pages#home'
+  # get '/pages/home' => 'pages#home'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
